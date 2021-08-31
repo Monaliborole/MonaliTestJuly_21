066c008d4ccb60b6aa68f69322c2289eb8bc0709
Monali Bendale <monaliborole31@gmail.com>
2021-08-31 23:53:58 +0530

    project upload

--- src/gitConflicts/Test_1.java
+++ src/gitConflicts/Test_1.java
@@ -1,5 +1,7 @@
 package gitConflicts;
 
 public class Test_1 {
-
+	public static void main(String[] args) {
+		System.out.println("Test1 ");
+	}
 }
--- src/gitConflicts/Test_2.java
+++ src/gitConflicts/Test_2.java
@@ -2,4 +2,7 @@ package gitConflicts;
 
 public class Test_2 {
 
+	public static void main(String[] args) {
+		System.out.println("Test2");
+	}
 }
