bd8e2eb94a01a382aeedfc415ad0e2bf40e4544c
Monali Bendale <monaliborole31@gmail.com>
2021-09-01 01:00:14 +0530

    Branch B Changes

--- src/gitConflicts/Test_2.java
+++ src/gitConflicts/Test_2.java
@@ -3,6 +3,7 @@ package gitConflicts;
 public class Test_2 {
 
 	public static void main(String[] args) {
-		System.out.println("Test2");
+		System.out.println("Test2 456");
+		System.out.println("New line for branch b");
 	}
 }
